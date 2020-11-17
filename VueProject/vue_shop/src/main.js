62d0e2c0f041555675da91d6ea1d444f9c6d6a48
Tan_rui_Feng <824449964@qq.com>
2020-11-17 15:37:57 +0800

    debug

--- VueProject/vue_shop/src/main.js
+++ VueProject/vue_shop/src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 
-createApp(App).use(router).mount('#app')
+Vue.config.productionTip = false
+new Vue({
+    router,
+    render: h=>h(App)
+}).$mount('#app')
